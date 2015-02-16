94629360e7b6ffdcb43652cd322398b7592c8130
Shaohua Zhou <shaohua@uber.com>
2015-02-16 14:18:59 -0800

    First pass

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # phabhub
 A Phabricator Chrome Extension to make it a little bit easier to use. Let's try to hit github level at least.
+
+# to use
+```
+bower install
+```
---
+++ src/inject/inject.js
@@ -0,0 +1,20 @@
+'use strict';
+
+chrome.extension.sendMessage({}, function(response) {
+  var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
+
+    // ----------------------------------------------------------
+    // This part of the script triggers when page is done loading
+    console.log("Hello. This message was sent from scripts/inject.js");
+    // ----------------------------------------------------------
+
+  }
+  }, 10);
+});
+
+$(document).ready(function(){
+  $('.phabricator-nav-content')
+    .prepend('<div class="phabhub-homepage-container aphront-panel-plain aphront-panel-view">Test</div>');
+});
